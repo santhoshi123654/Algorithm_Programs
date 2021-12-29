eee7b77bdcccd529fa8e78d0f5bf5852b66c6033
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-29 10:18:47 +0530

    Added:Insertion Sort.

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,37 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+
+		String string = "An,algorithm,is,a,set,of,instructions,for,solving,logical,and,mathematical,problems";
+		String[] stringArray = string.split(",");
+
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("\n1 for search \n2 for insertion sorting ");
+		int num = scanner.nextInt();
+
+		switch (num) {
+		case 1:
+			System.out.println("enter a word to search");
+			String str = scanner.next();
+			int result = SortingAlgorithm.binarySearch(stringArray, str);
+
+			if (result == -1)
+				System.out.println("Element not present");
+			else
+				System.out.println("Element found at " + "index " + result);
+			break;
+		case 2 :
+			SortingAlgorithm.insertionSort(stringArray);
+			break;
+		default :
+			System.out.println("enter valid case number ");
+		}
+	}
+}
